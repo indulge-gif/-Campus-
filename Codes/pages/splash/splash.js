cb695d5448b337d9ca1e2deb54fc8b7415ab77dc
徐德衡 <3360180645@qq.com>
2026-01-20 11:10:57 +0800

    Add files via upload

--- Codes/pages/splash/splash.js
+++ Codes/pages/splash/splash.js
@@ -1,1 +1,32 @@
+Page({
+  data: {
+    countdownText: '5秒后自动进入',
+    timer: null
+  },
 
+  onLoad() {
+    this.startCountdown()
+  },
+
+  startCountdown() {
+    let seconds = 5
+    this.setData({ countdownText: `${seconds}秒后自动进入` })
+    
+    this.data.timer = setInterval(() => {
+      seconds--
+      this.setData({ countdownText: `${seconds}秒后自动进入` })
+      
+      if (seconds <= 0) {
+        clearInterval(this.data.timer)
+        this.navigateToHome()
+      }
+    }, 1000)
+  },
+
+  navigateToHome() {
+    clearInterval(this.data.timer)
+    wx.redirectTo({
+      url: '/pages/home/home'
+    })
+  }
+})
